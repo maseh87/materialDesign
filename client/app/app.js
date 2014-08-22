f879b193afaaf96cdcdb5aea9c19ce3d50fc5aea
Mason <maseh87@gmail.com>
2014-08-22 16:23:35 -0700

    new directive made

--- client/app/app.js
+++ client/app/app.js
@@ -1,7 +1,77 @@
-angular.module('Material', []);
+angular.module('Material', [
+  'ui.router'
+])
 
 
-// .config(function($stateProvider) {
-//   $stateProvider
-//     .state();
-// });
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('app',{
+      url: '/'
+    });
+  $urlRouterProvider.otherwise('/');
+})
+
+.controller('Controller', function($scope) {
+  $scope.data = {
+    columns: [
+      ['Xianhui', 30, 200, 100, 400, 150, 250],
+      ['Josh', 50, 20, 10, 40, 15, 25]
+    ],
+    type: 'line'
+  };
+})
+.directive('lineChart', function() {
+  var chartIdCounter = Math.floor((Math.random()*1000)+1);
+  return {
+    restrict: 'E',
+    link: function(scope, element, attrs) {
+       //Assigning id to the element
+      var chartId;
+      if(element.attr('id')) {
+        chartId = element.attr('id');
+      }
+      else {
+        chartId = 'c3-chart-' + chartIdCounter;
+        element.attr('id', chartId);
+        chartIdCounter += 1;
+      }
+
+      //Preparing chart data and options
+      var genData = {
+        bindto: '#' + chartId,
+        data: scope.data
+      };
+      console.log(genData, ' genData');
+      genData.data.type = attrs.chart? attrs.chart : scope.data.type? scope.data.type : 'line';
+      if(scope.options) {
+        Object.keys(scope.options).forEach(function(key) {
+          genData[key] = scope.options[key];
+        });
+      }
+      //On data change, reload chart
+      onDataChanged = function(data, oldData) {
+        if(chart) {
+          chart.load(data);
+          console.log(data.columns.length);
+          console.log(oldData.columns.length);
+          if(data.columns.length < oldData.columns.length) {
+            console.log('data' + oldData.columns.length);
+            chart.unload(['data' + oldData.columns.length]);
+          }
+        }
+      };
+      scope.$watch('data', onDataChanged, true);
+
+      //On chart type change, reload chart
+      onChartChanged = function(chart) {
+        if(chart) {
+          scope.data.type = chart;
+          chart.load(data);
+        }
+      };
+      scope.$watch(function() {return attrs.chart; }, onChartChanged);
+      //Generating the chart
+      var chart = c3.generate(genData);
+    }
+  };
+});
